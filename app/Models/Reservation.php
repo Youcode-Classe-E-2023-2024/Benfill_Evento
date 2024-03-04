44266d37f78b2352012356e76ede78c48d1709ea
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-04 19:08:01 +0100

    Create Reservation.php

---
+++ app/Models/Reservation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'event_id',
+      'user_id',
+      'status'
+    ];
+
+    function event() {
+        return $this->hasMany(Event::class);
+    }
+
+    function user() {
+        return $this->belongsTo(User::class);
+    }
+}
