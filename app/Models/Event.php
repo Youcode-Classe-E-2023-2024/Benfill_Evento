2fdd6379ce362bc8cc1e0248986e836be1acb301
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-04 19:07:57 +0100

    Create Event.php

---
+++ app/Models/Event.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'picture',
+        'event_location',
+        'category_id',
+        'places',
+        'price',
+        'organizer',
+        'status',
+        'date'
+    ];
+
+    function user() {
+        return $this->belongsTo(User::class);
+    }
+    function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    function reservation() {
+        return $this->belongsTo(Reservation::class);
+    }
+}
