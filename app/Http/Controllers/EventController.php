1922d84b640ee822a91fe430918bd8f41ff3bf2d
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-07 21:09:35 +0100

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Event;
 use App\Models\Location;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
 
 class EventController extends Controller
@@ -14,7 +16,9 @@ class EventController extends Controller
     {
         $categories = Category::all();
         $locations = Location::all();
-        return view('pages.front_office.event', compact('categories', 'locations'));
+        $events = Event::with('category', 'user', 'location')->get();
+        $eventsRequest = Event::where('status', 'unconfirmed')->with('category', 'user', 'location')->get();
+        return view('pages.back_office.events', compact('categories', 'locations', 'events', 'eventsRequest'));
     }
 
     function create()
@@ -24,19 +28,20 @@ class EventController extends Controller
 
     function store(Request $request)
     {
+        $imagePath = $request->file('picture')->store('/events/profiles/', 'public');
         Event::create([
             'title' => $request['title'],
             'description' => $request['description'],
-            'picture' => $request['picture'],
-            'event_location' => $request['location'],
+            'picture' => $imagePath,
+            'location_id' => $request['location'],
             'category_id' => $request['category'],
             'places' => $request['places'],
             'price' => $request['price'],
-            'organizer' => $request['organizer'],
-            'status' => $request['satus'],
+            'user_id' => Auth::id(),
             'date' => $request['date'],
             'slug' => Str::slug($request['title'], '-')
         ]);
+        return back()->with('status', "success");
     }
 
     function show($slug)
@@ -46,7 +51,8 @@ class EventController extends Controller
         return view('pages.front_office.eventContent', compact('event'));
     }
 
-    function update(Request $request, $id) {
+    function update(Request $request, $id)
+    {
         $event = Event::find($id);
         $event->update([
             'title' => $request['title'],
@@ -57,8 +63,25 @@ class EventController extends Controller
             'places' => $request['places'],
             'price' => $request['price'],
             'organizer' => $request['organizer'],
-            'status' => $request['satus'],
             'date' => $request['date'],
         ]);
+    }
+
+    function changeStatus(Request $request, $id)
+    {
+        $event = Event::find($id);
+        if (isset($request->accept)) {
+            to_route('events.destroy', $event->id);
+        } else {
+            $event->update(['status' => "published"]);
+        }
+        return back();
+    }
+
+    function destroy($id)
+    {
+        $event = Event::find($id);
+        $event->delete();
+        return back();
     }
 }
