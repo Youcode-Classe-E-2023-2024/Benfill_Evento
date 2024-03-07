aa5f78fadbb648a1125ab2d5f0be869ffaec2caa
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-07 21:11:30 +0100

    Create manage_roles.blade.php

---
+++ resources/views/pages/back_office/manage_roles.blade.php
@@ -0,0 +1,14 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<link rel="stylesheet" href="{{ url('/css/dashboard.css') }}">
+
+<x-dashboard.headerdash/>
+<x-dashboard.sidebardash/>
+<x-dashboard.manage_roles/>
+
+
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet"/>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
+
+<script src="{{ url('/js/dashboard.js') }}"></script>
