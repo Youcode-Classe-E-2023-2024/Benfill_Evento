92da8af9f163189d6226a97dbc5a8c6b11c182da
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-07 21:10:26 +0100

    Update LocationSeeder.php

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -5,23 +5,29 @@ namespace Database\Seeders;
 use App\Models\Location;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
 
-class LocationsSeeder extends Seeder
+class LocationSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $locations = [
+        $apiUrl = 'https://raw.githubusercontent.com/alaouy/sql-moroccan-cities/master/json/ville.json';
 
-        ];
+        $response = Http::get($apiUrl);
+        if ($response->successful()) {
+            $locationsData = $response->json();
+            foreach ($locationsData as $location) {
+                Location::create([
+                    'location' => $location['ville']
+                ]);
+            }
 
-        foreach($locations as $location) {
-            Location::create([
-                'location' => $location
-            ]);
-
+            $this->command->info('Locations seeded successfully.');
+        } else {
+            $this->command->error('Failed to fetch data from the API.');
         }
     }
 }
