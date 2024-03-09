74aee3096da05492ab1cd375f339eb30557df228
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 21:27:30 +0100

    Update getPicUrl.php

--- app/Helpers/getPicUrl.php
+++ app/Helpers/getPicUrl.php
@@ -4,3 +4,25 @@ function getPicUrl($fileName, $folder)
     $picPath = "storage/$folder/$fileName";
     return asset($picPath);
 }
+
+function convertTimeFormat($inputTime, $option)
+{
+
+    if ($option === 'd') {
+        // Convert input time to Unix timestamp
+        $timestamp = strtotime($inputTime);
+
+        // Check if the conversion was successful
+        if ($timestamp === false) {
+            return "Invalid input time format";
+        }
+
+        // Format the timestamp to the desired output format
+        $outputFormat = date("l, F j Y", $timestamp);
+
+    } else {
+        $timestamp = strtotime($inputTime);
+        $outputFormat = date("Y-m-d\TH:i", $timestamp);
+    }
+    return $outputFormat;
+}
