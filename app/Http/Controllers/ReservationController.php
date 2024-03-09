3c0f9cda4056a135a9bda99b42d7d4bd6b5f7eb1
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 01:22:55 +0100

    Update ReservationController.php

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
+    function store(Request $request)
+    {
+        $eventId = $request->eventId;
+        $event = Event::find($eventId);
+        $event->reservationCount .= 1;
+        $event->save();
+    }
 }
