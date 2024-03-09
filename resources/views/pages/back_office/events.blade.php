489e9b962831b83f3ff523b019dd4bdd2fe98e19
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 21:23:24 +0100

    Update events.blade.php

--- resources/views/pages/back_office/events.blade.php
+++ resources/views/pages/back_office/events.blade.php
@@ -371,6 +371,16 @@
                     </div>
 
                     <div class="sm:col-span-1">
+                        <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Validation</label>
+                        <select name="validation" id="location"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="{{old('validation')}}" selected disabled hidden>Choose Validation Option here</option>
+                            <option value="auto" selected>Automatic</option>
+                            <option value="manuel">Manual</option>
+                        </select>
+                    </div>
+
+                    <div class="sm:col-span-1">
                         <label for="location" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
                         <select name="location" id="location"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
