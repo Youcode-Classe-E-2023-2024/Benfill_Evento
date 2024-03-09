cc391a1aab31e1a9b3a355a1103c04692ca4ccb4
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 01:24:03 +0100

    Update home.blade.php

--- resources/views/pages/front_office/home.blade.php
+++ resources/views/pages/front_office/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-    <x-home.categories/>
-    <x-home.gallery/>
+    <x-home.categories :categories="$categories"/>
+    <x-home.gallery :gallery="$gallery"/>
     <x-home.card :events="$events"/>
-
+    <script src="{{ asset('js/filter.js') }}"></script>
 @endsection
