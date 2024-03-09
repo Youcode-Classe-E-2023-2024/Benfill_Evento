5832fbf3f295a1653fd6f278a5bf52bf7549a42a
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 01:22:52 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function index() {
-        $events = Event::where('status', 'published')->get();
+    function index()
+    {
+        $events = Event::where('status', 'published')->paginate(10);
+        $categories = Category::all();
+        $gallery = Event::where('status', 'published')->orderBy('reservationCount', 'desc')
+            ->take(5)
+            ->get();
+        $gallery = count($gallery) != 0 ? $gallery : Event::where('status', 'published')->take(5)->get();
         return view('pages.front_office.home',
-        [
-            'events' => $events,
-
-        ]);
+            [
+                'events' => $events,
+                'categories' => $categories,
+                'gallery' => $gallery
+            ]);
     }
 }
