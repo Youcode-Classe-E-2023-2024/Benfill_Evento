5109d9879b1c3acee5ab3b3d4b18563d2bf018b0
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 21:27:01 +0100

    Update Ticket.php

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Ticket extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'reservation_id',
+        'qr_code'
+    ];
+
+    function event() {
+        return $this->belongsTo(Event::class);
+    }
+
+    function reservation() {
+        return $this->belongsTo(Reservation::class);
+    }
+
+    function user() {
+        return $this->belongsTo(User::class);
+    }
 }
