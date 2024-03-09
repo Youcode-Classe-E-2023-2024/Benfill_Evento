01c268ebafc4b6010df69ef296f06c961d15629d
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 01:24:22 +0100

    Update categories.blade.php

--- resources/views/components/home/categories.blade.php
+++ resources/views/components/home/categories.blade.php
@@ -1,11 +1,10 @@
-<div class="overflow-x-auto"<div class="overflow-x-auto">
+<div class="overflow-x-auto">
     <div class="flex space-x-4 p-4 justify-center">
-        <!-- Replace these items with your actual categories -->
-        <div class="bg-white text-gray-700 px-4 py-2 rounded font-sans">Category 1</div>
-        <div class="bg-white text-gray-700 px-4 py-2 rounded font-sans">Category 2</div>
-        <div class="bg-white text-gray-700 px-4 py-2 rounded font-sans">Category 3</div>
-        <div class="bg-white text-gray-700 px-4 py-2 rounded font-sans">Category 4</div>
-        <div class="bg-white text-gray-700 px-4 py-2 rounded font-sans">Category 5</div>
-        <!-- Add more categories as needed -->
+        <div id="homeBtn" class="hover:text-gray-900 cursor-pointer text-gray-700 px-4 py-2 rounded font-sans">Home
+        </div>
+        @foreach($categories as $category)
+            <div data-category-id="{{$category['id']}}"
+                 class="hover:text-gray-900 categorySelect bg-white cursor-pointer text-gray-700 px-4 py-2 rounded font-sans">{{$category['category']}}</div>
+        @endforeach
     </div>
 </div>
