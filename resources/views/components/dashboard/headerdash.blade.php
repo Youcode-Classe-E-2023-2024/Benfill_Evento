dce7b3971b2122baad29ab79732b5cf59d2e0f97
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 21:23:51 +0100

    Update headerdash.blade.php

--- resources/views/components/dashboard/headerdash.blade.php
+++ resources/views/components/dashboard/headerdash.blade.php
@@ -1,7 +1,7 @@
 
 <div class="fixed top-0 w-full flex items-center justify-between h-14 text-white" style="z-index: 10000;">
     <div class="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-blue-800 dark:bg-gray-800 border-none">
-        <img class="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="{{ asset('storage/profiles/'. auth()->user()->picture) }}" />
+        <img class="w-7 h-7 md:w-10 md:h-10 mr-2 rounded-md overflow-hidden" src="{{ asset('storage/'. auth()->user()->picture) }}" />
         <span class="hidden md:block">{{ auth()->user()->name }}</span>
     </div>
     <div class="flex justify-between items-center h-14 bg-blue-800 dark:bg-gray-800 header-right">
