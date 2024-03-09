d77aa60e81ac5257b1cf1cecd0062e4cc415dfa3
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-09 21:23:33 +0100

    Update header.blade.php

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,10 +1,21 @@
+<style>
+    .logo {
+        font-family: "Dancing Script", cursive;
+        font-optical-sizing: auto;
+        font-weight: 700;
+        font-style: normal;
+        font-size: 39px;
+    }
+
+</style>
+
 <nav class="bg-white border-gray-200 border-b-2">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo"/>
-            <span class="self-center text-2xl font-semibold whitespace-nowrap">Flowbite</span>
+        <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <h1 class="self-center text-2xl text-blue-500 font-semibold whitespace-nowrap logo">Evento</h1>
         </a>
         <div class="flex">
+            {{-- Search --}}
             <div class="flex md:order-2 mx-6">
                 <div class="relative hidden md:block">
                     <div
@@ -21,36 +32,53 @@
                            placeholder="Search...">
                 </div>
             </div>
+
+            {{-- Settingd --}}
             <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button"
-                        class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
-                        id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-                        data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo">
-                </button>
-                <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
-                     id="user-dropdown">
-                    <div class="px-4 py-3">
-                        <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                        <span class="block text-sm  text-gray-500 truncate">name@flowbite.com</span>
+                @if(\Illuminate\Support\Facades\Auth::check())
+
+                    <button type="button"
+                            class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
+                            id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
+                            data-dropdown-placement="bottom">
+                        <span class="sr-only">Open user menu</span>
+                        <img class="w-8 h-8 rounded-full" src="{{getPicUrl(getUser()->picture, 'pictures/profiles')}}"
+                             alt="user photo"></button>
+                    <!-- Dropdown menu -->
+                    <div
+                        class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
+                        id="user-dropdown">
+                        <div class="px-4 py-3">
+                            <span class="block text-sm text-gray-900">{{getUser()->name}}</span>
+                            <span class="block text-sm  text-gray-500 truncate">{{getUser()->email}}</span>
+                        </div>
+                        <ul class="py-2" aria-labelledby="user-menu-button">
+                            <li>
+                                <a href="/dashboard" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100e">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="/myTickets"
+                                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Tickets</a>
+                            </li>
+                            <li>
+                                <a href="/myReservation"
+                                   class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Reservations</a>
+                            </li>
+                            <li>
+
+                                <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">
+                                    Sign
+                                    out
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <ul class="py-2" aria-labelledby="user-menu-button">
-                        <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100e">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Earnings</a>
-                        </li>
-                        <li>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign out</a>
-                        </li>
-                    </ul>
-                </div>
+                @else
+                    <a href="{{route('login')}}"
+                       class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        Login
+                    </a>
+                @endif
             </div>
         </div>
     </div>
