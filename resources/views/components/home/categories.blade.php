b8b4371911d39fbbda35492f8b8bb0571dd75a37
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-10 16:32:14 +0000

    Update categories.blade.php

--- resources/views/components/home/categories.blade.php
+++ resources/views/components/home/categories.blade.php
@@ -1,10 +1,10 @@
 <div class="overflow-x-auto">
     <div class="flex space-x-4 p-4 justify-center">
-        <div id="homeBtn" class="hover:text-gray-900 cursor-pointer text-gray-700 px-4 py-2 rounded font-sans">Home
+        <div id="homeBtn" class="font-bold hover:text-blue-500 cursor-pointer text-gray-700 px-4 py-2 rounded font-sans">Home
         </div>
         @foreach($categories as $category)
             <div data-category-id="{{$category['id']}}"
-                 class="hover:text-gray-900 categorySelect bg-white cursor-pointer text-gray-700 px-4 py-2 rounded font-sans">{{$category['category']}}</div>
+                 class="font-bold hover:text-blue-500 categorySelect bg-white cursor-pointer text-gray-700 px-4 py-2 rounded font-sans">{{$category['category']}}</div>
         @endforeach
     </div>
 </div>
