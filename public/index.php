f881c71300f40cd6ebbe4675d94de02cb004c7fa
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-10 23:59:24 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
-
+ini_set('max_execution_time', '300');
+ini_set('memory_limit', '128M');
 define('LARAVEL_START', microtime(true));
 
 /*
