0228a78f0f39571083279fdf22c4be8418eb6453
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-10 23:59:35 +0000

    Update event-content.blade.php

--- resources/views/components/event-content.blade.php
+++ resources/views/components/event-content.blade.php
@@ -2,7 +2,8 @@
     <h1 class="text-blue-900 font-bold text-[30px]">Unveiling the Magic: Details for the '{{$event->title  }}'
         Experience</h1>
 </div>
-<div class="flex flex-wrap md h-screen mt-6 pr-6 border-gray-200 border-b-2">
+@dd($event->validation)
+<div class="flex flex-wrap md h-screen my-6 pr-6 border-gray-200 border-b-2">
     <div class="bg-white w-full md:w-1/2 h-screen">
         <div class="mx-32 border-2 bg-blueGray-900 flex px-6 flex-col justify-center">
             <div class="flex w-full justify-center mt-6">
@@ -14,40 +15,50 @@
                 </span>
                 </button>
             </div>
-            <h1 class="text-6xl font-bold mt-16">{{$event['title']}}</h1>
+            @if($event->validation != 'auto')
+                <div class="flex w-full justify-center mt-6">
+                <span
+                    class="inline-flex items-center m-2 px-3 py-1 bg-yellow-200 hover:bg-yellow-300 rounded-full text-sm font-semibold text-yellow-600">
+                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path
+                        d="M0 0h24v24H0V0z"
+                        fill="none"/><path
+                        d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/></svg>
+                <span class="ml-1">
+                  Require Confirmation
+                </span>
+              </span>
+                </div>
+            @endif
+            <h1 class="text-6xl font-bold text-center mt-16">{{$event['title']}}</h1>
 
-            <h5 class="mt-6">Quickly!! Buy your tickets</h5>
+            <h5 class="mt-6 text-center text-red-500 font-bold text-[30px]">Quickly!! Buy your tickets</h5>
 
             <!-- country region island -->
-            <div class="flex mt-16 font-light text-gray-500">
+            <div class="flex flex-col mt-16 font-light text-gray-500">
                 <div class="pr-4">
-                    <span class="uppercase">Country</span>
-                    <p class="text-2xl text-gray-900 font-semibold pt-2">Japan</p>
+                    <p class="text-2xl text-center text-gray-900 font-semibold pt-2">
+                        In {{$event->location->location}}</p>
                 </div>
                 <div class="pr-4">
-                    <span class="uppercase">Region</span>
-                    <p class="text-2xl text-gray-900 font-semibold pt-2">Kanto</p>
+                    <p class="text-l text-gray-900 text-center font-semibold pt-2">
+                        At {{convertTimeFormat($event->date, "d")}}</p>
                 </div>
-                <div class="pr-4">
-                    <span class="uppercase">island</span>
-                    <p class="text-2xl text-gray-900 font-semibold pt-2">Honshu</p>
+                <div class="pr-4 mt-6">
+                    <p class="text-2xl text-gray-900 text-center font-semibold pt-2">{{$event->price}} MAD</p>
+                </div>
+                <div class="pr-4 mt-6">
+                    <p class="text-2xl text-gray-900 text-center font-semibold pt-2"><span
+                            class="@if($event->places <= 10) text-red-500 @else text-green-500 @endif">{{$event->places}}</span>
+                        Available Places</p>
                 </div>
             </div>
-
-            <!-- description -->
-            <div
-                class="description w-full sm: md:w-2/3 mt-16 text-gray-500 text-sm"
-            >
-                {{$event['description']}}
-            </div>
-
             @if($event->places !== 0)
                 <a href="{{route('reserve', $event->slug)}}"
                    class="bg-green-500 py-6 text-center mb-6 text-white border-green-700 uppercase mt-5 text-sm font-semibold hover:underline">
                     buy now!
                 </a>
             @else
-                    <h3 class="text-center font-bold text-red-500">Sold Out</h3>
+                <h3 class="text-center font-bold text-red-500">Sold Out</h3>
                 <button
                     type="button"
                     class="cursor-not-allowed bg-gray-500 py-6 text-center mb-6 text-white border-green-700 uppercase mt-5 text-sm font-semibold hover:underline">
@@ -64,8 +75,11 @@
         />
     </div>
 </div>
-<div class="mb-[100px] mt-12 px-20">
-    <h2>Description</h2>
+<div class="mb-[100px] mt-32 px-20">
+    <div>
+        <h2 class="font-bold pb-6">Description</h2>
+        <hr class="my-6">
+    </div>
     <div>
         <p>
             {{$event->description}}
@@ -73,6 +87,9 @@
     </div>
 </div>
 
-<div class="flex justify-center items-center w-full">
-    <h1>Other Events</h1>
+<div class="flex flex-col justify-center items-center w-full">
+    <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse mb-6">
+        <h1 class="self-center text-2xl text-blue-500 font-semibold whitespace-nowrap logo">Evento</h1>
+    </a>
+    <h1 class="font-bold">Other Events</h1>
 </div>
