b4f17041ed48f90c0a1cbc3bda50e7fe5613f297
Anass Benfillous <anasse.benfillous@gmail.com>
2024-03-10 16:31:42 +0000

    Update home_gallery.php

--- app/View/Components/home_gallery.php
+++ app/View/Components/home_gallery.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class home.gallery extends Component
+class home_gallery extends Component
 {
     /**
      * Create a new component instance.
